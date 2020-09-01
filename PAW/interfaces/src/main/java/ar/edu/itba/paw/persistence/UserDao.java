2f287d3702f748880ba13dd36ed8b94ef84f5f42
Euge <epineiro@itba.edu.ar>
2020-09-01 00:38:08 -0300

    added user persistence

---
+++ PAW/interfaces/src/main/java/ar/edu/itba/paw/persistence/UserDao.java
@@ -0,0 +1,7 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.models.User;
+
+public interface UserDao {
+    public User findById(long id);
+}
--- PAW/services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
+++ PAW/services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
@@ -1,16 +1,19 @@
 package ar.edu.itba.paw.services;
 
 import ar.edu.itba.paw.models.User;
-import ar.edu.itba.paw.interfaces.UserService;
+import ar.edu.itba.paw.persistence.UserDao;
+import ar.edu.itba.paw.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-    public UserServiceImpl() {
-    }
+
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public User findById(int id) {
-        return new User(id,"PAW");
+        return userDao.findById(id);
     }
 }
