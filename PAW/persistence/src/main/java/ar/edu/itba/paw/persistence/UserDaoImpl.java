88743bd805e37aee5ca2a3487a2036a6fb6a62b0
Euge <epineiro@itba.edu.ar>
2020-09-01 01:12:17 -0300

    added persistence that was ignored

---
+++ PAW/persistence/src/main/java/ar/edu/itba/paw/persistence/UserDaoImpl.java
@@ -0,0 +1,16 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.models.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public User findById(long id){
+        return new User(id, "PAW from DAO");
+    }
+
+
+
+}
