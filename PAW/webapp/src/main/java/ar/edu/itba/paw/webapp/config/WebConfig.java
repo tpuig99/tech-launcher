347b3be438cbef5c27b0cc74f2c01022c3f40c8a
Ignacio <isampedro@itba.edu.ar>
2020-09-02 14:24:19 -0300

    First idea of css and bootstrap integration

--- PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -5,13 +5,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @ComponentScan({ "ar.edu.itba.paw.webapp.controller","ar.edu.itba.paw.services", "ar.edu.itba.paw.persistence", })
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
     @Bean
     public ViewResolver viewResolver() {
         final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -21,6 +23,14 @@ public class WebConfig {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        registry
+                .addResourceHandler("/styles/**")
+                .addResourceLocations("/styles/");
     }
 }
 
