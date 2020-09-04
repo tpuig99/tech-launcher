0ead50574ead53807da5e019d94e233124f6c451
Tamara <tamypuig@gmail.com>
2020-09-03 22:05:47 -0300

    Connecting project with testing database.

--- PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -3,12 +3,15 @@ package ar.edu.itba.paw.webapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.sql.DataSource;
 
 @EnableWebMvc
 @ComponentScan({ "ar.edu.itba.paw.webapp.controller","ar.edu.itba.paw.services", "ar.edu.itba.paw.persistence", })
@@ -31,6 +34,15 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry
                 .addResourceHandler("/styles/**")
                 .addResourceLocations("/styles/");
+    }
+    @Bean
+    public DataSource dataSource() {
+        final SimpleDriverDataSource ds = new SimpleDriverDataSource();
+        ds.setDriverClass(org.postgresql.Driver.class);
+        ds.setUrl("jdbc:postgresql://ec2-52-22-216-69.compute-1.amazonaws.com:5432/d86l7934jmqblf");
+        ds.setUsername("gbhbqhgxqihynh");
+        ds.setPassword("959523c108bbcbbf94a86d25428acea2051845a79486cc83f71163d39ee834b9");
+        return ds;
     }
 }
 
