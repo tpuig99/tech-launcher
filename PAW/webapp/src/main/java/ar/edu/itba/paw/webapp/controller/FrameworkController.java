03aa04b7a9772bbec2653c159ff140ebc6629232
Euge <epineiro@itba.edu.ar>
2020-09-03 22:32:32 -0300

    added frameworkController parameters

--- PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/FrameworkController.java
+++ PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/FrameworkController.java
@@ -4,6 +4,7 @@ import ar.edu.itba.paw.service.FrameworkService;
 import ar.edu.itba.paw.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,10 +14,10 @@ public class FrameworkController {
     @Autowired
     private FrameworkService fs;
 
-    @RequestMapping("/framework")
-    public ModelAndView framework() {
+    @RequestMapping("/frameworks/{id}")
+    public ModelAndView framework(@PathVariable long id) {
         final ModelAndView mav = new ModelAndView("frameworks/framework");
-        mav.addObject("framework", "angular");
+        mav.addObject("framework", fs.findById(id));
         return mav;
     }
 }
--- PAW/webapp/src/main/webapp/WEB-INF/jsp/frameworks/framework.jsp
+++ PAW/webapp/src/main/webapp/WEB-INF/jsp/frameworks/framework.jsp
@@ -19,8 +19,8 @@
 
             <div class="content">
 
-                <div class="title"> <h2>Nombre</h2> </div>
-                <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
+                <div class="title"> <h2>${framework.frameworkname}</h2> </div>
+                <div>${framework.description}</div>
 
                 <div>
                    <h4>Bibliografía</h4>
