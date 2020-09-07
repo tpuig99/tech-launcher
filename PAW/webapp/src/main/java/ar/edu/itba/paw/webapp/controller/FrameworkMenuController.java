d160f97bd3a6c0944c84bde0fd02b43304be8327
Ignacio <isampedro@itba.edu.ar>
2020-09-06 23:50:56 -0300

    Frameworks Menu
    Frameworks menu .jsp and controller added.

---
+++ PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/FrameworkMenuController.java
@@ -0,0 +1,27 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import ar.edu.itba.paw.models.ContentTypes;
+import ar.edu.itba.paw.models.Framework;
+import ar.edu.itba.paw.models.FrameworkCategories;
+import ar.edu.itba.paw.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FrameworkMenuController {
+
+    @Autowired
+    private FrameworkService fs;
+
+    @RequestMapping("/{category}")
+    public ModelAndView framework(@PathVariable String category) {
+        final ModelAndView mav = new ModelAndView("frameworks/frameworks_menu");
+        mav.addObject("category",category);
+        mav.addObject("frameworksList", fs.getByCategory(FrameworkCategories.getByName(category)));
+
+        return mav;
+    }
+}
