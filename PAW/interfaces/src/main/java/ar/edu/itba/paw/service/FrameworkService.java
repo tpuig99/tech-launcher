89db076be14381fdc8f2ace8aa88e077f5a4b5bd
Tamara <tamypuig@gmail.com>
2020-09-07 23:13:07 -0300

    Comments and Content logic added to Framework Service.

--- PAW/interfaces/src/main/java/ar/edu/itba/paw/service/FrameworkService.java
+++ PAW/interfaces/src/main/java/ar/edu/itba/paw/service/FrameworkService.java
@@ -1,11 +1,9 @@
 package ar.edu.itba.paw.service;
 
-import ar.edu.itba.paw.models.Comment;
-import ar.edu.itba.paw.models.Content;
-import ar.edu.itba.paw.models.Framework;
-import ar.edu.itba.paw.models.FrameworkCategories;
+import ar.edu.itba.paw.models.*;
 
 import java.awt.*;
+import java.net.URL;
 import java.util.List;
 
 public interface FrameworkService {
@@ -17,4 +15,10 @@ public interface FrameworkService {
     List<Content> getContent(long frameworkId);
     int getVotesCant(long frameworkId);
     List<Framework> getCompetitors(Framework framework);
+    Content insertContent(long frameworkId, long userId, String title, URL url, ContentTypes type);
+    int deleteContent(long contentId);
+    Content changeContent(long contentId, String title, URL url, ContentTypes types);
+    Comment insertComment(long frameworkId, long userId, String description);
+    int deleteComment(long commentId);
+    Comment changeComment(long commentId, String description);
 }
--- PAW/services/src/main/java/ar/edu/itba/paw/services/FrameworkServiceImpl.java
+++ PAW/services/src/main/java/ar/edu/itba/paw/services/FrameworkServiceImpl.java
@@ -3,6 +3,8 @@ package ar.edu.itba.paw.services;
 import ar.edu.itba.paw.models.*;
 import ar.edu.itba.paw.persistence.FrameworkDao;
 import ar.edu.itba.paw.persistence.VoteDao;
+import ar.edu.itba.paw.service.CommentService;
+import ar.edu.itba.paw.service.ContentService;
 import ar.edu.itba.paw.service.FrameworkService;
 import ar.edu.itba.paw.service.VoteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.awt.*;
+import java.net.URL;
 import java.util.List;
 
 @Service
@@ -21,6 +24,10 @@ public class FrameworkServiceImpl implements FrameworkService {
 
     @Autowired
     private VoteService vote;
+    @Autowired
+    private CommentService cmts;
+    @Autowired
+    private ContentService ctns;
 
     @Override
     public Framework findById(long id) {
@@ -62,12 +69,12 @@ public class FrameworkServiceImpl implements FrameworkService {
 
     @Override
     public List<Comment> getComments(long id) {
-        return null;
+        return cmts.getCommentsByFramework(id);
     }
 
     @Override
     public List<Content> getContent(long id) {
-        return null;
+        return ctns.getContentByFramework(id);
     }
 
     @Override
@@ -81,6 +88,37 @@ public class FrameworkServiceImpl implements FrameworkService {
         competitors.remove(framework);
         return competitors;
     }
+
+    @Override
+    public Content insertContent(long frameworkId, long userId, String title, URL url, ContentTypes type) {
+        return ctns.insertContent(frameworkId,userId,title,url,type);
+    }
+
+    @Override
+    public int deleteContent(long contentId) {
+        return ctns.deleteContent(contentId);
+    }
+
+    @Override
+    public Content changeContent(long contentId, String title, URL url, ContentTypes types) {
+        return ctns.changeContent(contentId,title,url,types);
+    }
+
+    @Override
+    public Comment insertComment(long frameworkId, long userId, String description) {
+        return cmts.insertComment(frameworkId,userId,description,-1);
+    }
+
+    @Override
+    public int deleteComment(long commentId) {
+        return cmts.deleteComment(commentId);
+    }
+
+    @Override
+    public Comment changeComment(long commentId, String description) {
+        return cmts.changeComment(commentId,description);
+    }
+
     private void getStarsAndVotes(Framework framework){
         List<Vote>votes =  vote.getByFramework(framework.getId());
         if(votes==null){
