51c593d053241cfb5976934207d970ddc534cd4d
Euge <epineiro@itba.edu.ar>
2020-09-08 00:13:23 -0300

    changed pathVariable to RequestParam

--- PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/FrameworkListController.java
+++ PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/FrameworkListController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,8 +15,9 @@ public class FrameworkListController {
     @Autowired
     private FrameworkService fs;
 
-    @RequestMapping("/search/{toSearch}")
-    public ModelAndView matchedFrameworks(@PathVariable String toSearch) {
+    @RequestMapping(path={"/search"}, method= RequestMethod.GET)
+    public ModelAndView matchedFrameworks(@RequestParam("toSearch") final String toSearch) {
+
         final ModelAndView mav = new ModelAndView("frameworks/frameworks_list");
 
         mav.addObject("matchingFrameworks", fs.getByNameOrCategory(toSearch));
