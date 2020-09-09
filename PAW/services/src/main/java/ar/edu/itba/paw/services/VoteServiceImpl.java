b89872f4ffc1a10374d9730c09427d1bfd82ad24
Tamara <tamypuig@gmail.com>
2020-09-08 21:11:29 -0300

    Vote Service
    Now, when a user votes again a framework it updates the stars.

--- PAW/services/src/main/java/ar/edu/itba/paw/services/VoteServiceImpl.java
+++ PAW/services/src/main/java/ar/edu/itba/paw/services/VoteServiceImpl.java
@@ -11,39 +11,43 @@ import java.util.List;
 @Service
 public class VoteServiceImpl implements VoteService {
     @Autowired
-    VoteDao vote;
+    VoteDao vs;
     @Override
     public List<Vote> getAll() {
-        return vote.getAll();
+        return vs.getAll();
     }
 
     @Override
     public List<Vote> getByFramework(long frameworkId) {
-        return vote.getByFramework(frameworkId);
+        return vs.getByFramework(frameworkId);
     }
 
     @Override
     public Vote getById(long voteId) {
-        return vote.getById(voteId);
+        return vs.getById(voteId);
     }
 
     @Override
     public Vote getByFrameworkAndUser(long frameworkId, long userId) {
-        return vote.getByFrameworkAndUser(frameworkId,userId);
+        return vs.getByFrameworkAndUser(frameworkId,userId);
     }
 
     @Override
     public Vote insert(long frameworkId, long userId, int stars) {
-        return vote.insert(frameworkId,userId,stars);
+        Vote vote = getByFrameworkAndUser(frameworkId,userId);
+        if(vote!=null){
+            update(vote.getVoteId(),stars);
+        }
+        return vs.insert(frameworkId,userId,stars);
     }
 
     @Override
     public int delete(long voteId) {
-        return vote.delete(voteId);
+        return vs.delete(voteId);
     }
 
     @Override
     public Vote update(long voteId, int stars) {
-        return vote.update(voteId,stars);
+        return vs.update(voteId,stars);
     }
 }
