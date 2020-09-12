b7a1643930e90a6b0fb54aebadb160bcb5c90cd6
Tamara <tamypuig@gmail.com>
2020-09-12 16:20:40 -0300

    Not sending email.

--- PAW/services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
+++ PAW/services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
@@ -10,6 +10,9 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
 import java.util.UUID;
 
 @Service
@@ -21,11 +24,15 @@ public class EmailServiceImpl implements EmailService {
     private UserService service;
 
     @Override
-    public void sendEmailConfirmation(User user,String appUrl) {
+    public void sendEmailConfirmation(User user,String appUrl){
         String token = UUID.randomUUID().toString();
         service.createVerificationToken(user, token);
+        try{
+        Transport.send(registrationMessage(user.getMail(),appUrl,token));}
+        catch (Exception e){
 
-        String recipientAddress = user.getMail();
+        }
+        /*String recipientAddress = user.getMail();
         String subject = "Registration Confirmation";
         String confirmationUrl = appUrl + "/regitrationConfirm.html?token=" + token;
         String message = "Click here to confirm your account!";
@@ -34,6 +41,43 @@ public class EmailServiceImpl implements EmailService {
         email.setTo(recipientAddress);
         email.setSubject(subject);
         email.setText(message + "\r\n" + "http://localhost:8080" + confirmationUrl);
-        mailSender.send(email);
+
+        mailSender.send(email);*/
     }
+
+    private Message registrationMessage(String mail, String appUrl, String token) throws MessagingException {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.mailtrap.io");
+        prop.put("mail.smtp.port", "25");
+        prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("confirmemailonly", "pawserver");
+            }
+        });
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("confirmemailonly@gmail.com"));
+        InternetAddress[] auxi = InternetAddress.parse(mail);
+        message.setRecipients(
+                Message.RecipientType.TO, InternetAddress.parse(mail));
+        message.setSubject("Registration Confirmation");
+        System.out.println(auxi[0].getAddress());
+        String confirmationUrl = appUrl + "/regitrationConfirm.html?token=" + token;
+        String initMessage = "Click here to confirm your account!";
+
+        String msg = initMessage + "\r\n" + "http://localhost:8080" + confirmationUrl;
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(msg, "text/html");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+
+        message.setContent(multipart);
+        return message;
+    }
+
 }
