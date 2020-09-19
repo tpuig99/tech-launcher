b6a18d55807036f31e5dc27d7999a9d13516884d
Tamara <tamypuig@gmail.com>
2020-09-19 17:25:04 -0300

    Adding Techs Types.

--- PAW/models/src/main/java/ar/edu/itba/paw/models/FrameworkType.java
+++ PAW/models/src/main/java/ar/edu/itba/paw/models/FrameworkType.java
@@ -1,12 +1,23 @@
 package ar.edu.itba.paw.models;
 
 public enum FrameworkType {
-    Online_Plataform("Online plataform"),
+    Online_Plataform("Online Plataform"),
     Framework("Framework"),
     Service("Service"),
     Database_System("Database System"),
     Programming_Languages("Programming Languages"),
-    Operating_System("Operating System");
+    Operating_System("Operating System"),
+    Runtime_Plataform("Runtime Plataform"),
+    Libraries("Libraries"),
+    Engine("Engine"),
+    Shell("Shell"),
+    Terminal("Terminal"),
+    Application("Application"),
+    Text_Editor("Text Editor"),
+    CSS_Modifier("CSS Modifier"),
+    API("API"),
+    Toolkit("Toolkit"),
+    IDE("IDE");
     private String type;
     FrameworkType(String type) {
         this.type=type;
