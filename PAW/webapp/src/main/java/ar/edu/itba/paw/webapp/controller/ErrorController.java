119a93ed54d9894f82963206d053a674acbed4ef
Tamara <tamypuig@gmail.com>
2020-09-23 17:08:13 -0300

    Error fixed.

--- PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
+++ PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -4,6 +4,7 @@ import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,7 +22,7 @@ public class ErrorController {
         private static final String ERROR_VIEW = "/error";
 
         public static ModelAndView redirectToErrorView() { return new ModelAndView("redirect:" + ERROR_VIEW); }
-
+        @RequestMapping("/error")
         @ExceptionHandler(value = Exception.class)
         public ModelAndView
         defaultErrorHandler(HttpServletRequest req, Exception e) {
@@ -37,8 +38,12 @@ public class ErrorController {
             ModelAndView mav = new ModelAndView("error");
             mav.addObject("exception", e);
             mav.addObject("url", req.getRequestURL());
-            User user = us.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName()).get();
-            mav.addObject("user_isMod", user.isVerify() || user.isAdmin());
+            mav.addObject("user", SecurityContextHolder.getContext().getAuthentication());
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            if( us.findByUsername(username).isPresent()){
+                User user = us.findByUsername(username).get();
+                mav.addObject("user_isMod", user.isVerify() || user.isAdmin());
+            }
             return mav;
         }
 
@@ -54,4 +59,6 @@ public class ErrorController {
         }
         return mav;
     }
+
+
 }
