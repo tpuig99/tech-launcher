1394d2e3735b4a469882899363bf03a29b9f57d5
Lboccardi <lboccardi@itba.edu.ar>
2020-10-03 02:33:19 -0300

    Added "Uploading and Showing Image" feature in User Profile

--- PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/form/ProfileForm.java
+++ PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/form/ProfileForm.java
@@ -1,5 +1,7 @@
 package ar.edu.itba.paw.webapp.form;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
