7e11e3d58c9d0ad8dd517633d377eea0ef50cb4a
Tamara <tamypuig@gmail.com>
2020-10-06 01:52:00 -0300

    Transactionals added on content.

--- PAW/services/src/main/java/ar/edu/itba/paw/services/ContentServiceImpl.java
+++ PAW/services/src/main/java/ar/edu/itba/paw/services/ContentServiceImpl.java
@@ -7,6 +7,7 @@ import ar.edu.itba.paw.persistence.ReportContentDao;
 import ar.edu.itba.paw.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.net.URL;
 import java.util.List;
@@ -24,52 +25,61 @@ public class ContentServiceImpl implements ContentService {
     @Autowired
     ReportContentDao rc;
 
+    @Transactional(readOnly = true)
     @Override
     public Optional<Content> getById(long contentId) {
         return content.getById(contentId);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Content> getContentByFramework(long frameworkId) {
         return content.getContentByFramework(frameworkId);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Content> getContentByFrameworkAndUser(long frameworkId, long userId) {
         return content.getContentByFrameworkAndUser(frameworkId, userId);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Content> getContentByUser(long userId) {
         return content.getContentByUser(userId);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Content> getContentByFrameworkAndType(long frameworkId, ContentTypes type) {
         return content.getContentByFrameworkAndType(frameworkId, type);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Content> getContentByFrameworkAndTypeAndTitle(long frameworkId, ContentTypes type, String title) {
         return content.getContentByFrameworkAndTypeAndTitle(frameworkId,type,title);
     }
 
-
+    @Transactional
     @Override
     public Content insertContent(long frameworkId, long userId, String title, String link, ContentTypes type) {
         return content.insertContent(frameworkId, userId, title, link, type);
     }
 
+    @Transactional
     @Override
     public int deleteContent(long contentId) {
         return content.deleteContent(contentId);
     }
 
+    @Transactional
     @Override
     public Optional<Content> changeContent(long contentId, String title, String link, ContentTypes types) {
         return content.changeContent(contentId, title, link, types);
     }
 
+    @Transactional
     @Override
     public void voteUp(long contentId, long userId) {
         Optional<ContentVote> vote = cv.getByContentAndUser(contentId,userId);
@@ -83,6 +93,7 @@ public class ContentServiceImpl implements ContentService {
         }
     }
 
+    @Transactional
     @Override
     public void voteDown(long contentId, long userId) {
         Optional<ContentVote> vote = cv.getByContentAndUser(contentId,userId);
@@ -97,45 +108,52 @@ public class ContentServiceImpl implements ContentService {
         Optional<Content> ct = content.getById(contentId);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Optional<ReportContent> getReporstById(long reportId) {
         return rc.getById(reportId);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<ReportContent> getAllReports() {
         return rc.getAll();
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<ReportContent> getReportsByFramework(long frameworkId) {
         return rc.getByFramework(frameworkId);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Optional<ReportContent> getReportsByContent(long contentId) {
         return rc.getByContent(contentId);
     }
 
+    @Transactional
     @Override
     public void addReport(long contentId, long userId, String description) {
         rc.add(contentId,userId,description);
     }
 
+    @Transactional
     @Override
     public void acceptReport(long contentId) {
         content.deleteContent(contentId);
     }
 
+    @Transactional
     @Override
     public void denyReport(long contentId) {
         rc.deleteByContent(contentId);
     }
 
+    @Transactional
     @Override
     public void deleteReport(long reportId) {
         rc.delete(reportId);
     }
-
 
 }
