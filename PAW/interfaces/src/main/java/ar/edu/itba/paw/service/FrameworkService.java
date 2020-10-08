1442945b3122ac0ab8ea8aeae2b055207c267380
Tamara <tamypuig@gmail.com>
2020-10-08 00:10:19 -0300

    2 params for stars done.

--- PAW/interfaces/src/main/java/ar/edu/itba/paw/service/FrameworkService.java
+++ PAW/interfaces/src/main/java/ar/edu/itba/paw/service/FrameworkService.java
@@ -21,7 +21,7 @@ public interface FrameworkService {
     List<Framework> getByMultipleTypes(List<FrameworkType> types);
     List<Framework> getByMinStars(int stars);
     List<Framework> getByUser(long userId);
-    List<Framework> search(String toSearch,List<FrameworkCategories> categories,List<FrameworkType> types,Integer stars,boolean nameFlag);
+    List<Framework> search(String toSearch, List<FrameworkCategories> categories, List<FrameworkType> types, Integer starsLeft,Integer starsRight, boolean nameFlag);
 
     void  orderByStars(List<Framework> frameworks, Integer order);
     void  orderByInteraction(List<Framework> frameworks, Integer order);
