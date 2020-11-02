43201ef2dd2ea77f5bd2ce4615527b61683e7fb5
Euge <epineiro@itba.edu.ar>
2020-11-02 18:22:11 -0300

    added PostComment Dao

--- PAW/persistence/src/main/java/ar/edu/itba/paw/persistence/PostCommentDaoImpl.java
+++ PAW/persistence/src/main/java/ar/edu/itba/paw/persistence/PostCommentDaoImpl.java
@@ -1,30 +1,50 @@
 package ar.edu.itba.paw.persistence;
 
-import ar.edu.itba.paw.models.PostComment;
+import ar.edu.itba.paw.models.*;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class PostCommentDaoImpl implements PostCommentDao{
+
+    @PersistenceContext
+    private EntityManager em;
+
+
     @Override
     public Optional<PostComment> findById(long postCommentId) {
-        return Optional.empty();
+        return Optional.ofNullable(em.find(PostComment.class, postCommentId));
     }
 
     @Override
     public List<PostComment> getByPost(long postId) {
-        return null;
+        final TypedQuery<PostComment> query = em.createQuery("from PostComment as pc where pc.post.id = :postId", PostComment.class);
+        query.setParameter("postId", postId);
+        return query.getResultList();
     }
 
     @Override
     public PostComment insertPostComment(long postId, long userId, String description, Long reference) {
-        return null;
+        final PostComment postComment = new PostComment();
+        Date ts = new Date(System.currentTimeMillis());
+        postComment.setPost(em.getReference(Post.class, postId));
+        postComment.setUser(em.getReference(User.class, userId));
+        postComment.setDescription(description);
+        postComment.setReference(reference);
+        postComment.setTimestamp(ts);
+        em.persist(postComment);
+        return postComment;
+
     }
 
     @Override
     public void deletePostComment(long postCommentId) {
-
+        em.remove(em.getReference(PostComment.class,postCommentId));
     }
 }
