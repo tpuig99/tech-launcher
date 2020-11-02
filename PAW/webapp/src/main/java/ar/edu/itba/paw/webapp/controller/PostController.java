a55ca4b18fa0501dc80d99ea94883b376ad19f81
Euge <epineiro@itba.edu.ar>
2020-11-02 20:30:03 -0300

    started PostController

---
+++ PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PostController.java
@@ -0,0 +1,26 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import ar.edu.itba.paw.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PostController {
+
+    @Autowired
+    PostService ps;
+
+    private final String START_PAGE = "1";
+
+    @RequestMapping("/posts")
+    public ModelAndView posts( @RequestParam(value = "posts_page", required = false, defaultValue = START_PAGE) Long postsPage) {
+
+        final ModelAndView mav = new ModelAndView("posts/posts_list");
+
+        return mav; 
+    }
+
+}
