94739d3c7d9bc4c35a0e897d4ae91e07b52b9f9d
Euge <epineiro@itba.edu.ar>
2020-11-03 22:49:33 -0300

    added user vars to PostController

--- PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PostController.java
+++ PAW/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PostController.java
@@ -2,10 +2,13 @@ package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.models.Framework;
 import ar.edu.itba.paw.models.Post;
+import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.service.PostService;
+import ar.edu.itba.paw.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +26,9 @@ public class PostController {
     @Autowired
     PostService ps;
 
+    @Autowired
+    private UserService us;
+
     private final String START_PAGE = "1";
     private final long POSTS_PAGE_SIZE = 10;
 
@@ -30,8 +36,16 @@ public class PostController {
     public ModelAndView posts( @RequestParam(value = "page", required = false, defaultValue = START_PAGE) Long postsPage) {
 
         final ModelAndView mav = new ModelAndView("posts/posts_list");
+        final Optional<User> optionalUser = us.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+
 
         mav.addObject("posts", ps.getAll(postsPage, POSTS_PAGE_SIZE) );
+
+        if( optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            mav.addObject("user", SecurityContextHolder.getContext().getAuthentication());
+            mav.addObject("user_isMod", user.isVerify() || user.isAdmin());
+        }
 
         return mav;
     }
