56f9c23b1d7c51325aba147fc1ab32ba3a4d91f3
Tamara <tamypuig@gmail.com>
2020-11-20 21:22:32 -0300

    search for post done

--- PAW/interfaces/src/main/java/ar/edu/itba/paw/persistence/PostDao.java
+++ PAW/interfaces/src/main/java/ar/edu/itba/paw/persistence/PostDao.java
@@ -1,7 +1,10 @@
 package ar.edu.itba.paw.persistence;
 
+import ar.edu.itba.paw.models.FrameworkCategories;
+import ar.edu.itba.paw.models.FrameworkType;
 import ar.edu.itba.paw.models.Post;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +15,7 @@ public interface PostDao {
     int getAmount();
     List<Post> getPostsByUser(long userId, long page, long pageSize);
     List<Post> getByTagName(String tagName, long page, long pageSize);
+    List<Post> search(String toSearch, List<String> tags, Integer starsLeft, Integer starsRight, Integer commentAmount, Date lastComment, Date lastUpdated, Integer order, long page, long pageSize);
 
     Post insertPost(long userId, String title, String description);
     void deletePost(long postId);
--- PAW/interfaces/src/main/java/ar/edu/itba/paw/service/PostService.java
+++ PAW/interfaces/src/main/java/ar/edu/itba/paw/service/PostService.java
@@ -1,8 +1,11 @@
 package ar.edu.itba.paw.service;
 
 import ar.edu.itba.paw.models.Comment;
+import ar.edu.itba.paw.models.FrameworkCategories;
+import ar.edu.itba.paw.models.FrameworkType;
 import ar.edu.itba.paw.models.Post;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +14,7 @@ public interface PostService {
     List<Post> getAll(long page, long pageSize);
     List<Post> getPostsByUser(long userId, long page, long pageSize);
     List<Post> getByTagName(String tagName, long page, long pageSize);
+    List<Post> search(String toSearch, List<String> tags, Integer starsLeft, Integer starsRight, Integer commentAmount, Date lastComment, Date lastUpdated, Integer order, long page, long pageSize);
 
     Post insertPost(long userId, String title, String description);
     void deletePost(long postId);
