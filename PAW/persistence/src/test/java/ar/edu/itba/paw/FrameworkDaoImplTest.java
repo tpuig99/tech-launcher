105cd055ed52e1a6c2778e4cb7219c90a79cfcc0
Euge <epineiro@itba.edu.ar>
2020-11-23 01:21:02 -0300

    started testing postDao

--- PAW/persistence/src/test/java/ar/edu/itba/paw/FrameworkDaoImplTest.java
+++ PAW/persistence/src/test/java/ar/edu/itba/paw/FrameworkDaoImplTest.java
@@ -1,49 +1,63 @@
 package ar.edu.itba.paw;
 
-import ar.edu.itba.paw.TestConfig;
 import ar.edu.itba.paw.models.*;
+import ar.edu.itba.paw.persistence.ContentDao;
+import ar.edu.itba.paw.persistence.FrameworkHibernateDaoImpl;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.jdbc.JdbcTestUtils;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.*;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
 
+@Rollback
+@Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 public class FrameworkDaoImplTest {
 
-  /*  private static final long FRAMEWORK_ID = 1;
+    private static final long FRAMEWORK_ID = 1;
     private static final String FRAMEWORK_NAME = "name";
-    private static final long USER_ID = 1;
     private static final FrameworkCategories CATEGORY = Enum.valueOf(FrameworkCategories.class, "Platforms");
     private static final String DESCRIPTION = "description";
     private static final String INTRODUCTION = "introduction";
-    private static final String LOGO = "logo";
     private static final FrameworkType TYPE = Enum.valueOf(FrameworkType.class, "Framework");
     private static final long AUTHOR = 1;
     private static final String AUTHOR_NAME = "user1";
     private static final byte[] PICTURE = null;
 
+    private static long USER_ID = 1;
+
+
     @Autowired
     private DataSource ds;
 
     @Autowired
-    private FrameworkDaoImpl frameworkDao;
+    private FrameworkHibernateDaoImpl frameworkDao;
+
+    @PersistenceContext
+    private EntityManager em;
 
     private JdbcTemplate jdbcTemplate;
     private SimpleJdbcInsert jdbcInsert;
@@ -51,7 +65,7 @@ public class FrameworkDaoImplTest {
     @Before
     public void setUp() {
         jdbcTemplate = new JdbcTemplate(ds);
-        this.jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+        jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
                 .withTableName("frameworks")
                 .usingGeneratedKeyColumns("framework_id");
 
@@ -60,8 +74,10 @@ public class FrameworkDaoImplTest {
         JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
 
         for (int i = 1; i < 6; i++) {
-            jdbcTemplate.execute("insert into users values("+i+",'user"+i+"','mail"+i+"',null,default,default,default,default)");
-            //  jdbcTemplate.execute("insert into content values("+i+",'user"+i+"','Title','link','book')");
+            User user = new User("user"+i,"mail"+i,null,true,"",true,null);
+            em.persist(user);
+            em.flush();
+            USER_ID = user.getId();
         }
     }
 
@@ -69,44 +85,43 @@ public class FrameworkDaoImplTest {
     public void testFindById() throws SQLException {
         //Preconditions
         JdbcTestUtils.deleteFromTables(jdbcTemplate, "frameworks");
-        Timestamp ts = new Timestamp(System.currentTimeMillis());
-        final Map<String, Object> args = new HashMap<>();
 
-        args.put("framework_id", FRAMEWORK_ID);
-        args.put("framework_name", FRAMEWORK_NAME);
-        args.put("category", CATEGORY.getNameCat());
-        args.put("description", DESCRIPTION);
-        args.put("introduction", INTRODUCTION);
-        args.put("logo", LOGO);
-        args.put("type", TYPE.getType());
-        args.put("date", ts);
-        args.put("author", AUTHOR);
+        Date date = new Date(System.currentTimeMillis());
 
-        Number frameworkId = jdbcInsert.executeAndReturnKey(args);
+        Framework framework = new Framework();
+        framework.setName(FRAMEWORK_NAME);
+        framework.setCategory(CATEGORY);
+        framework.setType(TYPE);
+        framework.setDescription(DESCRIPTION);
+        framework.setIntroduction(INTRODUCTION);
+        framework.setAuthor(em.find(User.class, USER_ID));
+        framework.setPublishDate(date);
+        em.persist(framework);
 
         //Class under test
-        Optional<Framework> framework = frameworkDao.findById(frameworkId.longValue());
+        Optional<Framework> framework2 = frameworkDao.findById(framework.getId());
+        em.flush();
 
         //Asserts
-        Assert.assertTrue(framework.isPresent());
-        Assert.assertEquals(FRAMEWORK_ID, framework.get().getId());
-        Assert.assertEquals(FRAMEWORK_NAME, framework.get().getName());
-        Assert.assertEquals(CATEGORY.getNameCat(), framework.get().getCategory());
-        Assert.assertEquals(DESCRIPTION, framework.get().getDescription());
-        Assert.assertEquals(INTRODUCTION, framework.get().getIntroduction());
-        Assert.assertEquals(LOGO, framework.get().getLogo());
-        Assert.assertEquals(TYPE.getType(), framework.get().getType());
-        Assert.assertEquals(AUTHOR_NAME, framework.get().getAuthor());
-        //Assert.assertNull(framework.get().getBase64image());
+        Assert.assertTrue(framework2.isPresent());
+        Assert.assertEquals(FRAMEWORK_NAME, framework2.get().getName());
+        Assert.assertEquals(CATEGORY, framework2.get().getCategory());
+        Assert.assertEquals(DESCRIPTION, framework2.get().getDescription());
+        Assert.assertEquals(INTRODUCTION, framework2.get().getIntroduction());
+        Assert.assertEquals(TYPE, framework2.get().getType());
+        Assert.assertEquals(USER_ID, framework2.get().getAuthor().getId().longValue());
+        Assert.assertEquals(date, framework2.get().getPublishDate());
+
     }
 
-    @Test
+   /* @Test
     public void tesFindByIdNotExists() {
         //Preconditions
         JdbcTestUtils.deleteFromTables(jdbcTemplate,"frameworks");
 
         //Class under test
         Optional<Framework> framework = frameworkDao.findById(FRAMEWORK_ID);
+        em.flush();
 
         //Asserts
         assertEquals(false,framework.isPresent());
@@ -118,18 +133,19 @@ public class FrameworkDaoImplTest {
         JdbcTestUtils.deleteFromTables(jdbcTemplate,"frameworks");
 
         //Class under test
-        Optional<Framework> framework = frameworkDao.create(FRAMEWORK_NAME,CATEGORY,DESCRIPTION, INTRODUCTION, TYPE,USER_ID,PICTURE);
+        final Optional<Framework> framework = frameworkDao.create(FRAMEWORK_NAME,CATEGORY,DESCRIPTION,INTRODUCTION,TYPE, USER_ID,PICTURE);
+        em.flush();
 
         //Asserts
         assertNotNull(framework);
         Assert.assertEquals(FRAMEWORK_ID, framework.get().getId());
         Assert.assertEquals(FRAMEWORK_NAME, framework.get().getName());
-        Assert.assertEquals(CATEGORY.getNameCat(), framework.get().getCategory());
+        Assert.assertEquals(CATEGORY.name(), framework.get().getCategory());
         Assert.assertEquals(DESCRIPTION, framework.get().getDescription());
         Assert.assertEquals(INTRODUCTION, framework.get().getIntroduction());
-        Assert.assertEquals(TYPE.getType(), framework.get().getType());
+        Assert.assertEquals(TYPE.name(), framework.get().getType());
         Assert.assertEquals(AUTHOR_NAME, framework.get().getAuthor());
-        Assert.assertNull(framework.get().getLogo());
+        Assert.assertNull(framework.get().getPicture());
         assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "frameworks","framework_id ="+framework.get().getId()));
     }
 
@@ -143,11 +159,10 @@ public class FrameworkDaoImplTest {
 
         args.put("framework_id", FRAMEWORK_ID);
         args.put("framework_name", FRAMEWORK_NAME);
-        args.put("category", CATEGORY.getNameCat());
+        args.put("category", CATEGORY.name());
         args.put("description", DESCRIPTION);
         args.put("introduction", INTRODUCTION);
-        args.put("logo", LOGO);
-        args.put("type", TYPE.getType());
+        args.put("type", TYPE.name());
         args.put("date", ts);
         args.put("author", AUTHOR);
 
@@ -170,11 +185,10 @@ public class FrameworkDaoImplTest {
             final Map<String, Object> args = new HashMap<>();
             args.put("framework_id", i);
             args.put("framework_name", FRAMEWORK_NAME+String.valueOf(i));
-            args.put("category", CATEGORY.getNameCat());
+            args.put("category", CATEGORY.name());
             args.put("description", DESCRIPTION);
             args.put("introduction", INTRODUCTION);
-            args.put("logo", LOGO);
-            args.put("type", TYPE.getType());
+            args.put("type", TYPE.name());
             args.put("date", ts);
             args.put("author", AUTHOR);
 
---
+++ PAW/persistence/src/test/java/ar/edu/itba/paw/PostDaoTest.java
@@ -0,0 +1,138 @@
+package ar.edu.itba.paw;
+
+
+import ar.edu.itba.paw.models.*;
+import ar.edu.itba.paw.persistence.PostDao;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static junit.framework.Assert.*;
+
+@Rollback
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+public class PostDaoTest {
+
+    private long USER_ID = 1;
+    private static final String TITLE = "title";
+    private static final String DESCRIPTION = "description";
+
+    @Autowired
+    private DataSource ds;
+
+    @Autowired
+    private PostDao postDao;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert jdbcInsert;
+
+    @Before
+    public void setUp() {
+        jdbcTemplate = new JdbcTemplate(ds);
+        jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("posts")
+                .usingGeneratedKeyColumns("post_id");
+
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
+
+        for (int i = 1; i < 6; i++) {
+            User user = new User("user"+i,"mail"+i,null,true,"",true,null);
+            em.persist(user);
+
+            em.flush();
+            USER_ID = user.getId();
+        }
+
+    }
+
+    @Test
+    public void testCreate() {
+        JdbcTestUtils.deleteFromTableWhere(jdbcTemplate,"posts","title = "+TITLE);
+
+        final Post post = postDao.insertPost(USER_ID, TITLE, DESCRIPTION);
+
+        em.flush();
+        assertNotNull(post);
+        assertEquals(USER_ID, post.getUser().getId().longValue());
+        assertEquals(TITLE,post.getTitle());
+        assertEquals(DESCRIPTION, post.getDescription());
+        assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "posts","post_id ="+post.getPostId()));
+    }
+
+    @Test(expected = Exception.class)
+    public void testCreateOnExisting() {
+        
+        JdbcTestUtils.deleteFromTableWhere(jdbcTemplate,"posts","title = ",TITLE);
+        
+        final Map<String, Object> args = new HashMap<>();
+        args.put("description", DESCRIPTION);
+        args.put("user_id",USER_ID);
+        args.put("title", TITLE);
+        jdbcInsert.execute(args);
+
+       final Post post = postDao.insertPost(USER_ID, TITLE, DESCRIPTION);
+    }
+
+    @Test
+    public void testDelete() {
+        JdbcTestUtils.deleteFromTableWhere(jdbcTemplate,"posts","title = "+TITLE);
+        Timestamp ts = new Timestamp(System.currentTimeMillis());
+
+        Post post  = new Post();
+        post.setUser(em.find(User.class,USER_ID));
+        post.setTitle(TITLE);
+        post.setDescription(DESCRIPTION);
+        post.setTimestamp(ts);
+        em.persist(post);
+
+        postDao.deletePost(post.getPostId());
+
+        em.flush();
+        assertEquals(0, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "posts","post_id ="+post.getPostId()));
+    }
+
+    @Test
+    public void testGetById() {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate,"posts");
+        Timestamp ts = new Timestamp(System.currentTimeMillis());
+
+        Post post  = new Post();
+        post.setUser(em.find(User.class,USER_ID));
+        post.setTitle(TITLE);
+        post.setDescription(DESCRIPTION);
+        post.setTimestamp(ts);
+        em.persist(post);
+
+        Optional<Post> post2 = postDao.findById(post.getPostId());
+
+        em.flush();
+        assertTrue(post2.isPresent());
+        assertEquals(USER_ID,post2.get().getUser().getId().longValue());
+        assertEquals(TITLE, post2.get().getTitle());
+        assertEquals(DESCRIPTION, post2.get().getDescription());
+    }
+
+
+}
