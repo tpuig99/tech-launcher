4701e29014a911fbce3221ee949c67730593b41d
Lboccardi <lboccardi@itba.edu.ar>
2020-11-24 12:17:05 -0300

    Added Tests for ReportComment and Modified Constraint

--- PAW/persistence/src/main/java/ar/edu/itba/paw/persistence/ReportCommentHibernateDaoImpl.java
+++ PAW/persistence/src/main/java/ar/edu/itba/paw/persistence/ReportCommentHibernateDaoImpl.java
@@ -9,9 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -60,7 +58,9 @@ public class ReportCommentHibernateDaoImpl implements ReportCommentDao{
     public void deleteReportByComment(long commentId) {
         final TypedQuery<Long> query = em.createQuery("select rc.reportId from ReportComment rc where rc.comment.commentId = :commentId", Long.class);
         query.setParameter("commentId", commentId);
-        delete(query.getSingleResult());
+        for (Long l : query.getResultList()) {
+            delete(l);
+        }
     }
 
     @Override
