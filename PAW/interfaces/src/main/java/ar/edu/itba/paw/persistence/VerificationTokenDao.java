b9377e322cb35dceddda138c1ecbe31f93d520dc
Ignacio <isampedro@itba.edu.ar>
2020-11-23 23:15:45 -0300

    Test TokenDao v1 done

--- PAW/interfaces/src/main/java/ar/edu/itba/paw/persistence/VerificationTokenDao.java
+++ PAW/interfaces/src/main/java/ar/edu/itba/paw/persistence/VerificationTokenDao.java
@@ -5,7 +5,7 @@ import ar.edu.itba.paw.models.VerificationToken;
 import java.util.Optional;
 
 public interface VerificationTokenDao {
-    void insert(long userId,String token);
-    void change(VerificationToken verificationToken, String token);
+    VerificationToken insert(long userId,String token);
+    VerificationToken change(VerificationToken verificationToken, String token);
     Optional<VerificationToken> getByToken(String token);
 }
