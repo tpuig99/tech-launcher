50446bc562ad311dc2365fcc997d0146aa575fbc
Euge <epineiro@itba.edu.ar>
2020-12-14 11:23:23 -0300

    added i18n to login

---
+++ PAW/frontend/app/scripts/i18n/translations.es.js
@@ -0,0 +1,26 @@
+'use strict';
+define([], function() {
+
+	return {
+		WELCOME_MESSAGE: 'Always a pleasure scaffolding your apps',
+
+		WELCOME_CONTROLLER: 'A new and shiny controller has been made!',
+		WELCOME_CONTROLLER_FOLLOWUP: 'This is the default view for your controlller. Change it (if you want, of course)!',
+
+    LOGIN_WREF:'Tech Launcher - Iniciar Sesión',
+    LOGIN_TITLE:'Iniciar Sesión',
+    LOGIN_ERROR:'El nombre de usuario o la contraseña son incorrectos.',
+    LOGIN_USERNAME:'Nombre de Usuario',
+    LOGIN_USERNAME_PLACEHOLDER:'nombre_usuario',
+    LOGIN_PASSWORD:'Contraseña',
+    LOGIN_PASSWORD_REPEAT:'Repetir Contraseña',
+    LOGIN_PASSWORD_PLACEHOLDER:'********',
+    LOGIN_EMAIL:'Email',
+    LOGIN_EMAIL_PLACEHOLDER:'example@google.com',
+    LOGIN_REMEMBER_ME:'Recordarme',
+    LOGIN_SIGN_UP_QUESTION:'¿No tienes una cuenta?',
+    LOGIN_SIGN_UP:'Registrarse',
+    LOGIN_FORGOT_QUESTION:'¿Olvidaste tu contraseña?',
+    LOGIN_FORGOT_PASSWORD:'Recuperar Contraseña',
+	};
+});
