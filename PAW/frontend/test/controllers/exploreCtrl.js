094f509200fc4c0e601fa300de2154771d82bb12
Euge <epineiro@itba.edu.ar>
2020-12-30 14:46:51 -0300

    chore karma and jasmine configuration

---
+++ PAW/frontend/test/controllers/exploreCtrl.js
@@ -0,0 +1,15 @@
+describe("sorting list of techs", function() {
+
+  // individual tests go here
+  it('sorts in descending order by default', function() {
+    // your test assertion goes here
+    expect(true).toEqual(true);
+  });
+
+  it('sorts in asscending order by default', function() {
+    // your test assertion goes here
+    expect(false).not.toEqual(true);
+  });
+
+
+});
