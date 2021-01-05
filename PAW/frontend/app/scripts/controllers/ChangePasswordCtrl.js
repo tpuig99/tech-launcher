17a478834d4ff0d0309478e214a8f07d033e27c9
Euge <epineiro@itba.edu.ar>
2021-01-05 20:39:26 -0300

    added formError color

--- PAW/frontend/app/scripts/controllers/ChangePasswordCtrl.js
+++ PAW/frontend/app/scripts/controllers/ChangePasswordCtrl.js
@@ -4,6 +4,22 @@ define(['frontend', 'services/sessionService'], function(frontend) {
 
     frontend.controller('ChangePasswordCtrl', function($scope, $routeParams, sessionService) {
       $scope.passwordChanged = false;
+
+      $scope.isPresent = false;
+      $scope.$parent.$watch('username',function () {
+        var user = sessionService.getStorageUser();
+        if (user !== undefined) {
+          sessionService.getCurrentUser(user.location).then(function (response) {
+            $scope.username = response.data.username;
+            $scope.isPresent = true;
+            $scope.userLocation = user.location;
+          });
+          $scope.getPosts();
+        }
+      });
+
+
+
       $scope.changePassword = function(newPassword) {
         sessionService.changePassword($routeParams.token, newPassword).then(function(response) {
           if (response.status === 200) {
